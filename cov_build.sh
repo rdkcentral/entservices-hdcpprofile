cb79c2fced021a23304bd7d200d83c846808a942
dkumar798 <dinesh_kumar2@comcast.com>
2026-02-12 01:40:09 +0530

    RDKEMW-13559: Integrate entservices-hdcpprofile to middleware builds (#1)
    * RDKEMW-13559: Integrate entservices-hdcpprofile to middleware builds
    
    * RDKEMW-13559: Integrate entservices-hdcpprofile to middleware builds
    
    * RDKEMW-13559: Integrate entservices-hdcpprofile to middleware builds
    
    * RDKEMW-13559: Integrate entservices-hdcpprofile to middleware builds
    
    * Update L1-tests.yml
    
    * Update cov_build.sh
    
    * Update CMakeLists.txt
    
    * Update CMakeLists.txt
    
    * Update CMakeLists.txt
    
    * Update CHANGELOG.md
    
    * Create CHANGELOG.md
    
    ---------
    
    Co-authored-by: DineshkumarJP <dineshitsoft@gmail.com>

--- cov_build.sh
+++ cov_build.sh
@@ -5,12 +5,12 @@ set -e
 GITHUB_WORKSPACE="${PWD}"
 ls -la ${GITHUB_WORKSPACE}
 ############################
-# Build entservices-inputoutput
+# Build entservices-hdcpprofile
 echo "======================================================================================"
-echo "buliding entservices-inputoutput"
+echo "buliding entservices-hdcpprofile"
 
 cd ${GITHUB_WORKSPACE}
-cmake -G Ninja -S "$GITHUB_WORKSPACE" -B build/entservices-inputoutput \
+cmake -G Ninja -S "$GITHUB_WORKSPACE" -B build/entservices-hdcpprofile \
 -DUSE_THUNDER_R4=ON \
 -DCMAKE_INSTALL_PREFIX="$GITHUB_WORKSPACE/install/usr" \
 -DCMAKE_MODULE_PATH="$GITHUB_WORKSPACE/install/tools/cmake" \
@@ -23,9 +23,7 @@ cmake -G Ninja -S "$GITHUB_WORKSPACE" -B build/entservices-inputoutput \
 -DRDK_SERVICES_COVERITY=ON \
 -DRDK_SERVICES_L1_TEST=ON \
 -DDS_FOUND=ON \
--DPLUGIN_HDMICECSOURCE=ON \
 -DPLUGIN_HDCPPROFILE=ON \
--DPLUGIN_HDMICECSINK=ON \
 -DCMAKE_CXX_FLAGS="-DEXCEPTIONS_ENABLE=ON \
 -I ${GITHUB_WORKSPACE}/entservices-testframework/Tests/headers \
 -I ${GITHUB_WORKSPACE}/entservices-testframework/Tests/headers/audiocapturemgr \
@@ -52,12 +50,8 @@ cmake -G Ninja -S "$GITHUB_WORKSPACE" -B build/entservices-inputoutput \
 -include ${GITHUB_WORKSPACE}/entservices-testframework/Tests/mocks/HdmiCec.h \
 -Wall -Wno-unused-result -Werror -Wno-error=format \
 -Wl,-wrap,system -Wl,-wrap,popen -Wl,-wrap,syslog \
--DENABLE_TELEMETRY_LOGGING -DUSE_IARMBUS \
--DENABLE_SYSTEM_GET_STORE_DEMO_LINK -DENABLE_DEEP_SLEEP \
--DENABLE_SET_WAKEUP_SRC_CONFIG -DENABLE_THERMAL_PROTECTION \
--DUSE_DRM_SCREENCAPTURE -DHAS_API_SYSTEM -DHAS_API_POWERSTATE \
--DHAS_RBUS -DDISABLE_SECURITY_TOKEN -DENABLE_DEVICE_MANUFACTURER_INFO -DUSE_THUNDER_R4 -DTHUNDER_VERSION=4 -DTHUNDER_VERSION_MAJOR=4 -DTHUNDER_VERSION_MINOR=4" \
+-DUSE_THUNDER_R4 -DTHUNDER_VERSION=4 -DTHUNDER_VERSION_MAJOR=4 -DTHUNDER_VERSION_MINOR=4" \
 
-cmake --build build/entservices-inputoutput --target install
+cmake --build build/entservices-hdcpprofile --target install
 echo "======================================================================================"
 exit 0
